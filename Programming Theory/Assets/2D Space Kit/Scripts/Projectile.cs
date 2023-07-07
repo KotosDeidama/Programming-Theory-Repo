4fcaf0f91187825a366cd7ecd9c85493058ed41a
KotosDeidama <gianmarco.prism@gmail.com>
2023-07-07 11:16:58 +0200

    Projectile moves

--- Programming Theory/Assets/2D Space Kit/Scripts/Projectile.cs
+++ Programming Theory/Assets/2D Space Kit/Scripts/Projectile.cs
@@ -1,20 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class Projectile : MonoBehaviour {
 	public GameObject shoot_effect;
 	public GameObject hit_effect;
 	public GameObject firing_ship;
+	private Rigidbody2D missileRigidbody2D;
+	[Range(0, 5000)] [SerializeField] private float missileSpeed;
 	
 	// Use this for initialization
 	void Start () {
 		firing_ship = GameObject.FindWithTag("Ship");
+		missileRigidbody2D = GetComponent<Rigidbody2D>();
 		GameObject obj = (GameObject) Instantiate(shoot_effect, transform.position  - new Vector3(0,0,5), Quaternion.identity); //Spawn muzzle flash
 		obj.transform.parent = firing_ship.transform;
-		//Destroy(gameObject, 5f); //Bullet will despawn after 5 seconds
+		Destroy(gameObject, 2f); //Bullet will despawn after 5 seconds
 	}
-	
-	
+
+	private void Update()
+	{
+		 missileRigidbody2D.velocity = transform.up * missileSpeed * Time.deltaTime;
+		//missileRigidbody2D.AddForce(transform.forward * missileSpeed* Time.deltaTime, ForceMode2D.Impulse);
+		
+	}
+
+
 	void OnTriggerEnter2D(Collider2D col) {
 		Debug.Log(col.gameObject.name);
 		//Don't want to collide with the ship that's shooting this thing, nor another projectile.
